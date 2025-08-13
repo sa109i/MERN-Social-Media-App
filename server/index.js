e01b3ca0b6304a963448d44338fe331387c98777
samuel <120247808+sa109i@users.noreply.github.com>
2025-08-13 16:34:28 +0300

    untracked .env

--- server/index.js
+++ server/index.js
@@ -14,18 +14,48 @@ import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(import.meta.filename);
-dotenv.config()
+
+dotenv.config({ path: '../.env' })
 
 const app = express()
 
 app.use(express.json())
 app.use(helmet())
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
-app.use(morgan("common"))
+app.use(morgan("tiny"))
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')))
 
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'public/assets')
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({ storage }) 
+
+
+app.post('/auth/register', upload.single('cover'), register)
+
+
+const PORT = process.env.PORT
+
+mongoose.connect(process.env.MONGO_URL)  
+    .then(() => {
+        console.log("connection established")
+        app.listen(PORT, () => {
+            console.log(`server is listening on ${PORT}`)
+        })
+    })
+    .catch(() => {
+        console.error("unable to connected to database!")
+    })
 
 
---
+++ server/models/user.js
@@ -0,0 +1,42 @@
+import { Schema, model } from "mongoose";
+
+const UserSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    friends: {
+        type: Array,
+        default: []
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+    },
+    picturePath: {
+        type: String,
+        default: ""
+    },
+    location: {
+        type: String,
+    },
+    occupation: {
+        type: String,
+    },
+    viewedProfile: {
+        type: Number,
+    },
+    impressions: {
+        type: Number,
+    }
+}, { timestamps: true })
+
+export default model('User', UserSchema)
